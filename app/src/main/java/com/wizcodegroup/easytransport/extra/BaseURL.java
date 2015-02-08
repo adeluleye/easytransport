f785f6a3612fd88eff084c8b6f2b3978987cf053
[adeluleye] <[abideen.adelu@meltwater.org]>
2015-02-08 08:01:21 +0000

    Update

--- app/src/main/java/com/wizcodegroup/easytransport/extra/BaseURL.java
+++
@@ -1,9 +0,0 @@
-package com.wizcodegroup.easytransport.extra;
-
-public class BaseURL {
-
-	public static String HTTP = "https://maps.googleapis.com/maps/api/place/";
-	public static String HTTPs = "https://maps.googleapis.com/maps/api/place/";
-	
-
-}
